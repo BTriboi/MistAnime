c7c43769df5741b23a0c2a3d831402caf79f8948
KHINKS <67277544+KHINKS@users.noreply.github.com>
2021-08-13 22:10:51 +0300

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const scapper = require('./scrapper')
+const scrapper = require('./scrapper')
 const express = require('express')
 const { env } = require('process')
 const cors = require('cors')
@@ -7,38 +7,38 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('👋 Hello world🌍, Welcome to 🦄 GogoAnime API 🧬 </br> Available routes : /Popular , /NewSeasons , /search/:query , /getAnime/:animeId , /getEpisode/:episodeId')
+    res.send('Hello world🌍! Welcome to 🦄 MistAnime API 🧬 </br></br> Available routes :  <b>/Popular</b> ,  <b>/NewSeasons</b> ,  <b>/search/:query</b> ,  <b>/getAnime/:animeId</b> ,  <b>/getEpisode/:episodeId</b> </br></br> <b>Note that /Popular and /NewSeasons must include tha page number, as in /Popular/1</b>')
 })
 
 app.get('/Popular/:page', async (req, res) => {
-    const result = await scapper.popular(req.params.page)
+    const result = await scrapper.popular(req.params.page)
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
 
 app.get('/NewSeasons/:page', async (req, res) => {
-    const result = await scapper.newSeason(req.params.page)
+    const result = await scrapper.newSeason(req.params.page)
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
 
 app.get('/search/:query', async (req, res) => {
 
-    const result = await scapper.search(req.params.query)
+    const result = await scrapper.search(req.params.query)
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
 
 app.get('/getAnime/:query', async (req, res) => {
 
-    const result = await scapper.anime(req.params.query)
+    const result = await scrapper.anime(req.params.query)
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
 
 app.get('/getEpisode/:query', async (req, res) => {
 
-    const result = await scapper.watchAnime(req.params.query)
+    const result = await scrapper.watchAnime(req.params.query)
     res.header("Content-Type", 'application/json');
     res.send(JSON.stringify(result, null, 4))
 })
